e4acd752095b7db11b3271aafa43b41715ead142
dyoshioka-555 <yoshioka.daiki@g.sp.m.is.nagoya-u.ac.jp>
2024-11-11 19:41:23 +0900

    data change

--- mos.js
+++ mos.js
@@ -53,9 +53,10 @@ function start_experiment() {
 
     var method_paths = [];
 
-    method_paths.push(wav_dir + "set" + set_num + "/a.list");
-    method_paths.push(wav_dir + "set" + set_num + "/b.list");
-    method_paths.push(wav_dir + "set" + set_num + "/b.list");
+    method_paths.push(wav_dir + "set" + set_num + "/ori.list");
+    method_paths.push(wav_dir + "set" + set_num + "/Fastspeech2.list");
+    method_paths.push(wav_dir + "set" + set_num + "/VITS.list");
+    method_paths.push(wav_dir + "set" + set_num + "/DVT.list");
 
     file_list = makeFileList(method_paths);
     console.log(file_list);
